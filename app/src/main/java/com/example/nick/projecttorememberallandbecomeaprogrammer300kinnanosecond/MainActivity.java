9249f6b6bddd909c1bf567b40e4d89221b825a6f
bbamrin <bbamrin@gmail.com>
2018-06-08 21:28:34 +0300

    1
    1

---
+++ app/src/main/java/com/example/nick/projecttorememberallandbecomeaprogrammer300kinnanosecond/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nick.projecttorememberallandbecomeaprogrammer300kinnanosecond;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
